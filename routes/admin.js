0935931f72b7e9bd6fd81fc73bf397f9fe20164f
Eric <ericbradshaw98@gmail.com>
2024-02-12 17:16:11 -0500

    added admin routes and changed query

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const router  = express.Router();
 const {getAllOrderItems} = require("../db/queries/orders");
-const {getAllOrderedItems} = require("../db/queries/ordered_items");
+
 
 //CREATE (POST TO CREATE MENU ITEM)
 //READ ALL
 router.get("/", (req, res) => {
-  Promise.all([
-  getAllOrderItems(10),
-  getAllOrderedItems(10)
+  getAllOrderItems(10)
+  .then(orders_items => {
 
-])
-  .then(([orders_items, ordered_items]) => {
-
-    res.render("admin", { orders_items, ordered_items });
+    res.render("admin", { orders_items });
   })
   .catch(e => {
     console.error(e);

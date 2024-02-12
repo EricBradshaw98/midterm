6be1da3f25438630472482afacead9d554287423
Eric Bradshaw <37272270+EricBradshaw98@users.noreply.github.com>
2024-02-12 15:06:50 -0500

    Add files via upload

--- db/seeds/04_orders_seeds.sql
+++ db/seeds/04_orders_seeds.sql
@@ -1,9 +1,9 @@
 -- Users table seeds here (Example)
 INSERT INTO orders (customer_id, order_placed, order_ready, active)
-VALUES (1, NOW(), DATE_ADD(NOW(), INTERVAL 40 MINUTE), true);
+VALUES (1, NOW(), CURRENT_DATE + INTERVAL '40 MINUTE', true);
 
 INSERT INTO orders (customer_id, order_placed, order_ready, active)
-VALUES (2, NOW(), DATE_ADD(NOW(), INTERVAL 60 MINUTE), true);
+VALUES (2, NOW(), CURRENT_DATE + INTERVAL '60 MINUTE', true);
 
 INSERT INTO orders (customer_id, order_placed, order_ready, active)
-VALUES (2, NOW(), DATE_ADD(NOW(), INTERVAL 60 MINUTE), true);
+VALUES (2, NOW(), CURRENT_DATE + INTERVAL '60 MINUTE', true);

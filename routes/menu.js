426a431b3c5d4103dfe7e5d2591eb600a559be12
Mikhail8425 <mikhailshemet@gmail.com>
2024-02-12 15:23:26 -0500

    menu display works

--- routes/menu.js
+++ routes/menu.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router  = express.Router();
+const {getAllMenuItems} = require("../db/queries/menu");
 
-router.get('/', (req, res) => {
-  res.render('menu');
-}); 
+//CREATE (POST TO CREATE MENU ITEM)
+//READ ALL
+router.get("/", (req, res) => {
+  getAllMenuItems(10)
+  .then(menu_items => {
+
+    res.render("menu", { menu_items });
+  })
+  .catch(e => {
+    console.error(e);
+    res.send(e);
+  });
+});
+
+
+// READ ONE
+//UPDATE
+//DELETE
+
+
+
 
 module.exports = router;

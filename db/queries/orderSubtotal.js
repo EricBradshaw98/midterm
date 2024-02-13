8fe6ec38a70f36b2f12ef24914897884ee49c098
Mikhail8425 <mikhailshemet@gmail.com>
2024-02-13 18:25:53 -0500

    orderSubtotal function plus header.html files

---
+++ db/queries/orderSubtotal.js
@@ -0,0 +1,21 @@
+const db = require('../connection');
+
+const orderSubtotal = function (orderId) {
+  const queryString = `
+  SELECT SUM(price * ordered_items.quantity)/100 AS subtotal
+  FROM menu_items
+  JOIN ordered_items on (menu_items.id = ordered_items.menu_item_id)
+  JOIN orders on (ordered_items.order_id = orders.id)
+  WHERE orders.id = $1
+  GROUP BY orders.id, price, ordered_items.quantity;
+  `;
+  return db.query(queryString, [orderId])
+  .then((data) => {
+    return data.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+}
+
+module.exports = {orderSubtotal}

fa56e5e3177aae4a13f858869d6a4482a2917188
Mikhail8425 <mikhailshemet@gmail.com>
2024-02-13 13:43:47 -0500

    we found a bug, additional reviews don't display on the review page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,27 +8,23 @@ $("document").ready(function() {
     const reviewContent = $('.leave_review #review-text').val()
     console.log('reviewContent', reviewContent)
 
-    alert(reviewContent);
-
     if (!reviewContent.trim()) {
       showError('🛑 ⚠️ Review content cannot be empty! ⚠️ 🛑');
       return;
     }
-
+  
     $.ajax({
-    url: "http://localhost:8080/reviews/submit",
-    context: document.body,
-    data: {'reviewContent': reviewContent},
-    method: "POST",
-    success: function() {
-      console.log('success')
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-      console.log('Problem saving tweet');
-    }
+      url: "http://localhost:8080/reviews/submit",
+      context: document.body,
+      data: {'reviewContent': reviewContent},
+      method: "POST",
+      success: function() {
+        console.log('success')
+      },
+      error: function() {
+        console.log('Problem saving review');
+      }
+    });
   });
-
-  });
-
   console.log('document ready done')
 });

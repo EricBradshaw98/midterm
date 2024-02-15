cb1829fe787dbd05f3b992c0f15163441af463bc
Mikhail8425 <mikhailshemet@gmail.com>
2024-02-14 19:04:08 -0500

    end of day push

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,96 +1,96 @@
 $(document).ready(function() {
 
-//addtocart
+  //addtocart
 
-$('.addToCartButton').on('click', function() {
-  $(this).closest('.addToCartForm').trigger("submit");
-  $(this).closest('.addToCartForm')[0].reset();
-});
-
-$('.addToCartForm').submit(function(e) {
-  e.preventDefault();
-  const newURL = $(this).attr("action");
-  const values = $(this).serialize();
-
-  $.ajax({
-    type: "POST",
-    url: newURL,
-    data: values,
-    success: (res) => {
-      console.log("success");
-    }
+  $('.addToCartButton').on('click', function() {
+    $(this).closest('.addToCartForm').trigger("submit");
+    $(this).closest('.addToCartForm')[0].reset();
   });
-});
 
-//removefromcart
+  $('.addToCartForm').submit(function(e) {
+    e.preventDefault();
+    const newURL = $(this).attr("action");
+    const values = $(this).serialize();
 
-$('.removeItemAjax').submit(function(e) {
-  e.preventDefault();
-  const newURL = $(this).attr("action");
-  const values = $(this).serialize();
-
-
-  $.ajax({
-    type: "POST",
-    url: newURL,
-    data: values,
-    success: (jsonData) => {
-      const currentSubtotal = jsonData.subtotal;
-      const newHTML = `<p>SUBTOTAL : $${currentSubtotal}</p>`;
-      $("subtotalTarget").empty().append(newHTML);
-    }
-});
-});
-
-//quantity add remove
-
-$('.quantityAjax').on("keyup keydown change", function() {
-  $(".update-form").trigger("submit");
-});
-
-
-$('.remove-btn').on('click', function() {
-  $(this).closest(".removeItemAjax").trigger("submit");
-  const $menuItem = $(this).closest('foodItem');
-  $menuItem.hide();
-});
-
-//update
-$('.update-form').submit(function(e) {
-  e.preventDefault();
-  const newURL = $(this).attr("action");
-  const values = $(this).serialize();
-
-
-  $.ajax({
-    type: "POST",
-    url: newURL,
-    data: values,
-    success: (jsonData) => {
-      const currentSubtotal = jsonData.subtotal;
-      const newHTML = `<p>SUBTOTAL : $${currentSubtotal}</p>`;
-      $("subtotalTarget").empty().append(newHTML);
-    }
+    $.ajax({
+      type: "POST",
+      url: newURL,
+      data: values,
+      success: (res) => {
+        console.log("success");
+      }
+    });
   });
-});
 
-//adjust quantity
+  //removefromcart
 
-//plus minus
-$('quantity-btn').on('click', function() {
-  if ($(this).hasClass('fa-plus')) {
-    const add = parseInt($(this).parent().find('quantityAjax').val()) +1;
-    $(this).parent().find('quantityAjax').val(add).trigger('change');
-  }
+  $('.removeItemAjax').submit(function(e) {
+    e.preventDefault();
+    const newURL = $(this).attr("action");
+    const values = $(this).serialize();
 
-  if ($(this).hasClass('fa-minus')) {
-    const remove = parseInt($(this).parent().find('quantityAjax').val()) -1;
 
-    if (remove === 0) {
-      remove = 1;
+    $.ajax({
+      type: "POST",
+      url: newURL,
+      data: values,
+      success: (jsonData) => {
+        const currentSubtotal = jsonData.subtotal;
+        const newHTML = `<p>SUBTOTAL : $${currentSubtotal}</p>`;
+        $("subtotalTarget").empty().append(newHTML);
+      }
+  });
+  });
+
+  //quantity add remove
+
+  $('.quantityAjax').on("keyup keydown change", function() {
+    $(".update-form").trigger("submit");
+  });
+
+
+  $('.remove-btn').on('click', function() {
+    $(this).closest(".removeItemAjax").trigger("submit");
+    const $menuItem = $(this).closest('foodItem');
+    $menuItem.hide();
+  });
+
+  //update
+  $('.update-form').submit(function(e) {
+    e.preventDefault();
+    const newURL = $(this).attr("action");
+    const values = $(this).serialize();
+
+
+    $.ajax({
+      type: "POST",
+      url: newURL,
+      data: values,
+      success: (jsonData) => {
+        const currentSubtotal = jsonData.subtotal;
+        const newHTML = `<p>SUBTOTAL : $${currentSubtotal}</p>`;
+        $("subtotalTarget").empty().append(newHTML);
+      }
+    });
+  });
+
+  //adjust quantity
+
+  //plus minus
+  $('quantity-btn').on('click', function() {
+    if ($(this).hasClass('fa-plus')) {
+      const add = parseInt($(this).parent().find('quantityAjax').val()) +1;
+      $(this).parent().find('quantityAjax').val(add).trigger('change');
     }
 
-    $(this).parent().find('quantityAjax').val(remove).trigger('change');
-  }
-  });
+    if ($(this).hasClass('fa-minus')) {
+      const remove = parseInt($(this).parent().find('quantityAjax').val()) -1;
+
+      if (remove === 0) {
+        remove = 1;
+      }
+
+      $(this).parent().find('quantityAjax').val(remove).trigger('change');
+    }
+    });
 });
--- routes/cart.js
+++ routes/cart.js
@@ -4,7 +4,6 @@ const db = require('../db/connection')
 const { Template } = require('ejs')
 const userQueries = require('../db/queries/users')
 // const client = require('./twilio-api')
-const { getAllFoodItems } = require("../db/queries/menu");
 
 //get for viewing
 
@@ -39,5 +38,53 @@ router.get('/', (req, res) => {
     res.send(err);
   })
 })
+
+router.post('/:orderID', (req, res) => {
+
+  const menuItemID = req.body.menuItemID;
+  const quantity = req.body.quantity;
+  const orderID = req.params.orderID;
+
+  userQueries.searchCart(orderID, menuItemID)
+    .then((data) => {
+      if (data.length === 0) {
+        // Food item not already in cart
+        userQueries.addToCart(orderID, menuItemID, quantity)
+          .then(() => res.redirect('/'));
+      }
+      if (data.length > 0) {
+        // Food item already in cart
+        userQueries.updateCart(quantity, orderID, menuItemID)
+          .then(() => res.redirect('/'));
+      }
+    });
+});
+
+// SUBMIT ORDER
+router.post('/submitOrder/:orderID', (req, res) => {
+  const orderID = req.params.orderID;
+  userQueries.submitOrder(orderID)
+    .then(() => {
+      userQueries.getOwnerPhone()
+        .then((phoneNumber) => {
+          //  Leave commented to save $$
+          client.messages
+            .create({
+              body: `Hello, there is a new online order: ${orderID}, please check Food Delivery App for details.`,
+              to: phoneNumber, // Text your number
+              from: '+14085604628', // From a valid Twilio number
+            })
+            .then((message) => {
+              console.log(message.sid);
+              console.log(message.body);
+            })
+            .catch((err) => console.log(err));
+          res.redirect('/orders');
+        });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
 module.exports = router;
